4263e211b18f7ba83b482d945324a138e1d64e74
fgvenancio <filipenancio@gmail.com>
2024-08-29 15:58:29 -0300

    crawler ok

--- Alura/crawler-python/app.py
+++ Alura/crawler-python/app.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 year = int(datetime.now().strftime('%Y'))
+month = int(datetime.now().strftime('%m'))
+time = 4 + (month/12)
+expect = 0.06
 
 print(year)
 
@@ -14,7 +17,8 @@ class Act:
     def toString(self):
         return f"{self.name} : {self.maxValue}" 
     
-setAct  = ['ggbr4']
+#setAct  = ['bbdc3', 'bbdc4', 'fiqe3', 'ggbr4', 'intb3', 'klbn11', 'sapr11', 'taee11', 'wege3']
+setAct = ['bbse3', 'vivt3', 'egie3', 'itub3']
 
 for act in setAct:
 
@@ -29,6 +33,8 @@ for act in setAct:
     title = soup.title.text
 
     soma = 0.
+    valor = 0.
+
     for child in soup.find_all('table')[1].children:
         for td in child:
             try:
@@ -36,11 +42,14 @@ for act in setAct:
                 arr.pop(0)
                 arr.pop(4)
                 y = int(arr[0])
-                if y > (year-5):
-                    #soma += float(arr[1])
+                if y > (year-time):
+                    soma += float(arr[1].replace(',','.'))
+                    if y==year: valor = float(arr[2][3:].replace(',','.'))
                     print(arr)
             except:
                 continue
+    
+    print(act + ' - R$ ' + str(valor) + ' >> R$ %.2f' %(soma/time/expect))
            
 
 
